c2b1226ef571feea28f7a178ce90d0267e887f9b
Casey McLewin <mclewinc@gmail.com>
2017-10-27 09:47:27 -0400

    Initial Commit

---
+++ Easy Capture/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Easy Capture
+//
+//  Created by Casey McLewin on 2017-10-27.
+//  Copyright © 2017 Casey McLewin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
